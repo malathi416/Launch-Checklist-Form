5fcbf7ec278dd6497c88985f47e2558b5ed647aa
Malathi416 <ammu.416@gmail.com>
2019-10-20 23:12:09 -0500

    changed the logic of the code behaviour

--- script.js
+++ script.js
@@ -27,38 +27,44 @@ window.addEventListener("load",function(){
       }else if(isNaN(CargoMass.value)){
          alert("Enter a CargoMass as Number");         
          return;
-      }      
-      if(FuelLevel.value< 10000){
+      } 
+       if((FuelLevel.value< 10000) && (CargoMass.value>10000)) {
          document.getElementById("faultyItems").style.visibility = "visible";
-         document.getElementById("pilotStatus").innerHTML = `${PilotName.value} is ready`;
-         document.getElementById("copilotStatus").innerHTML =`${CopilotName.value} is ready`;        
+         document.getElementById("pilotStatus").style.visibility = "hidden";
+         document.getElementById("copilotStatus").style.visibility = "hidden"; 
+         document.getElementById("fuelStatus").innerHTML="There is not enough fuel for the journey";     
+         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+         document.getElementById("launchStatus").style.color="red";
+         document.getElementById("cargoStatus").innerHTML="There is too much mass for the shuttle to take off"; 
+      }        
+      else if(FuelLevel.value< 10000){
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("pilotStatus").style.visibility = "hidden";
+         document.getElementById("copilotStatus").style.visibility = "hidden";      
          document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
          document.getElementById("launchStatus").style.color="red";
          document.getElementById("fuelStatus").innerHTML="There is not enough fuel for the journey";
-         // document.getElementById("cargoStatus").innerHTML="there is not enough fuel for the journey";
-         // TODO: Change color to Red
+         document.getElementById("cargoStatus").style.visibility = "hidden";         
 
       }else if(CargoMass.value>10000) {
          document.getElementById("faultyItems").style.visibility = "visible";
-         document.getElementById("pilotStatus").innerHTML = `${PilotName.value} is ready`;
-         document.getElementById("copilotStatus").innerHTML =`${CopilotName.value} is ready`;        
+         document.getElementById("pilotStatus").style.visibility = "hidden";
+         document.getElementById("copilotStatus").style.visibility = "hidden"; 
+         document.getElementById("fuelStatus").style.visibility = "hidden";       
          document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
          document.getElementById("launchStatus").style.color="red";
-         document.getElementById("cargoStatus").innerHTML="There is too much mass for the shuttle to take off";
-         
-         // TODO: Change color to Red             
-         
-      }else {
-         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("cargoStatus").innerHTML="There is too much mass for the shuttle to take off";           
+      }      
+      else  {
+         document.getElementById("faultyItems").style.visibility = "hidden";
          document.getElementById("pilotStatus").innerHTML = `${PilotName.value} is ready`;
          document.getElementById("copilotStatus").innerHTML =`${CopilotName.value} is ready`;        
          document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
          document.getElementById("launchStatus").style.color="green";        
-      }
+      
      
       fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-         response.json().then(function(json){
-            // console.log(json)
+         response.json().then(function(json){            
             let i = Math.floor(Math.random()*json.length);
             let div=document.getElementById("missionTarget");           
             div.innerHTML=`            
@@ -73,8 +79,9 @@ window.addEventListener("load",function(){
 <img src="${json[i].image}"/>`
          });
          })
+      }
    });
-   
+
    });
 
    /* This block of code shows how to format the HTML once you fetch some planetary JSON!
